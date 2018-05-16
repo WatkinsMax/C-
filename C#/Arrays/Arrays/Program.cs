1d89d8c9765e85f67884efdc2819c752c766c470
WatkinsMax <38860533+WatkinsMax@users.noreply.github.com>
2018-05-16 16:02:14 +0100

    Add files via upload

---
+++ C#/Arrays/Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[5];
+            double sum = 0;
+            double average;
+
+
+
+            Console.WriteLine("Please enter 5 numbers to be averaged.");
+           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+            Console.WriteLine("Please enter a number: ");
+                numbers [i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            average = sum / numbers.Length;
+            Console.WriteLine("The average is: " + average);
+
+        }//main
+    }//program
+}//namespace
---
+++ C#/Methods/Methods/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            char signChosen;
+            decimal result = 0;
+
+
+            Console.WriteLine("This program will do simple arithmatic for you if you enter two numbers and a sign. \n( + - * / )");
+            Console.WriteLine("Please enter the first number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the sign: ");
+            signChosen = Convert.ToChar(Console.ReadLine());
+            if (signChosen != "+", "-", "*", "/") 
+            {
+                Console.WriteLine("Please enter a valid sign");
+            } else {
+
+                Console.WriteLine("And finally, please selected the second number: ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (signChosen)
+            {
+                case '+':
+                result = AddNumbers(firstNumber, secondNumber);
+                    break;
+                case '-':
+                    result = SubtractNumbers(firstNumber, secondNumber);
+                    break;
+                case '*':
+                    result = MultiplyNumbers(firstNumber, secondNumber);
+                    break;
+                case '/':
+                    result = DivideNumbers(firstNumber, secondNumber);
+                    break;
+             }
+
+            Console.WriteLine("The answer is: " + result);
+
+        }//main
+
+        static decimal AddNumbers(int first, int second)
+        {
+            decimal result = (first + second);
+            return result;
+
+        }
+        static decimal SubtractNumbers(int first, int second)
+        {
+            decimal result = (first - second);
+            return result;
+
+        }
+        static decimal MultiplyNumbers(int first, int second)
+        {
+            decimal result = (first * second);
+            return result;
+
+        }
+        static decimal DivideNumbers(int first, int second)
+        {
+            decimal result = (first / second);
+            return result;
+
+        }//AddNumbers
+
+    }//program
+}//namespace
